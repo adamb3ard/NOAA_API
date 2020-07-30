c47bbc413415cc9a0a55b90e5c7e0ad65d2d3b66
Adam Beard <adam.beard96@icloud.com>
2020-07-29 19:25:37 -0500

    project init

---
+++ src/main/java/com/example/NOAA_API/NoaaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.NOAA_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoaaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoaaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NOAA_API/NoaaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NOAA_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoaaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
